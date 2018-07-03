281d8f9ec89d00d08227a873f43d1ad2c585096e
Dimitri <Dimitri16h@gmail.com>
2018-07-03 10:15:48 -0400

    Landing page in an ok state....

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -3,70 +3,18 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
-    <title>Fleet</title>
+    <title >Fleet</title>
 
+    <!-- Bootstrap 4.1.1-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-    <!-- Styles -->
-    <style>
-        html, body {
-            background-color: #fff;
-            color: #636b6f;
-            font-family: 'Raleway', sans-serif;
-            font-weight: 100;
-            height: 100vh;
-            margin: 0;
-        }
-
-        .full-height {
-            height: 100vh;
-        }
-
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
-
-        .position-ref {
-            position: relative;
-        }
-
-        .top-right {
-            position: absolute;
-            right: 10px;
-            top: 18px;
-        }
-
-        .content {
-            text-align: center;
-        }
-
-        .title {
-            font-size: 84px;
-        }
-
-        .links > a {
-            color: #636b6f;
-            padding: 0 25px;
-            font-size: 12px;
-            font-weight: 600;
-            letter-spacing: .1rem;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
-
-        .m-b-md {
-            margin-bottom: 30px;
-        }
-    </style>
+    <link href="{{ asset('css/landing.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Archivo+Black" rel="stylesheet">
 </head>
-<body class="bg-dark text-light">
+
+<body class="text-light text-center container-fluid">
 
     <!-- Top Navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -75,10 +23,11 @@
         </button>
 
         <div class="dropdown-menu bg-dark" id="navbarNavAltMarkup">
-            <a class="dropdown-item text-light" href="/">Home</a>
-            <a class="dropdown-item text-light" href="#">TODO</a>
-            <a class="dropdown-item text-light" href="#">TODO</a>
-            <a class="dropdown-item text-light disabled" href="#">TODO</a>
+            <a class="dropdown-item text-light active" href="/">Home</a>
+            <a class="dropdown-item text-light" href="#">Analytics</a>
+            <a class="dropdown-item text-light" href="#">Truck Management</a>
+            <a class="dropdown-item text-light disabled" href="#">IFTA Assistance</a>
+            <a class="dropdown-item text-light disabled" href="#">Invoicing</a>
         </div>
 
         @if (Route::has('login'))
@@ -86,7 +35,7 @@
                 @auth
                     <a href="{{ url('/home') }}" class="text-light">Dashboard</a>
                 @else
-                    <a href="{{ route('login') }}" class="text-light">Login</a>
+                    <a href="{{ route('login') }}" class="text-light p-3">Login</a>
                     <a href="{{ route('register') }}" class="text-light">Register</a>
                 @endauth
             </div>
@@ -95,27 +44,62 @@
 
 
 
-    <div class="title m-b-md h-50  text-center align-middle" 
-        style="
-            background-image: url(https://image.shutterstock.com/z/stock-photo-big-red-semi-truck-on-highway-748195210.jpg); 
-            background-position: center center;
-            background-size:100%;
-            display: flex;
-  justify-content: center;
-  align-items: center;">
-                <flex class="text-light">
-                    Fleet
-                </flex>
+    <!-- Title / Image Div -->
+    <div id="title-div" class="title">
+        <flex class="text-light">
+            Fleet
+        </flex>
     </div>
 
 
 
-    <nav class="flex-center position-ref links">
-            <a href="#">Analytics</a>
-            <a href="#">Load Management</a>
-            <a href="#">IFTA Assistance</a>
-            <a href="#">Invoicing</a>
-    </nav>
+
+
+    <div style="background-color: #03002e;" class="mt-1 p-3 text-center">
+        
+        <p id="questionText">What can we do for your company?<p>
+        
+        <div class="row justify-content-center" style="height: 108px;">
+            <button class="btn-secondary text-center p-1 m-2 col col-sm-3">
+                <img src="{{ asset('img/graph.png') }}"></img>
+                <div>Analytics</div>
+            </button>
+
+            <button class="btn-secondary text-center p-1 m-2 col col-sm-3">
+                <img src="{{ asset('img/truck.png') }}"></img>
+                <div>Truck Management</div>
+            </button>
+        </div>
+
+        <div class="row justify-content-center" style="height: 108px;">
+            <button class="btn-secondary text-center p-1 m-2 col col-sm-3">
+                <img src="{{ asset('img/taxes.png') }}"></img>
+                <div>IFTA Assistance</div>
+            </button>
+
+            <button class="btn-secondary text-center p-1 m-2 col col-sm-3">
+                <img src="{{ asset('img/invoice1.png') }}"></img>
+                <div>Invoicing</div>
+            </button>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
